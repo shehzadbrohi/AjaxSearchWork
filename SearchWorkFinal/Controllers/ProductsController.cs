bcca36888e4eb2790cfc6caf356d5517a656111d
shazy <shahzadbrohi@hotmail.com>
2017-09-15 20:07:52 +0500

    commit

---
+++ SearchWorkFinal/Controllers/ProductsController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using SearchWorkFinal.Models;
+
+namespace SearchWorkFinal.Controllers
+{
+    public class ProductsController : Controller
+    {
+        //
+        // GET: /Products/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Search(string keyword)
+        {
+            var db = new ShopDbEntities();
+            var products = db.Products.Where(p=> p.Name.Contains(keyword));
+
+
+            return PartialView(products);
+        }
+    }
+}
---
+++ SearchWorkFinal/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="SearchWorkFinal.MvcApplication" Language="C#" %>
